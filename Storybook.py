57f7348e003cb2e6b7fac4a3b6f0f7e23d1dbbb7
Karuru Midhun Sai <164469674+midhunsai04@users.noreply.github.com>
2024-08-07 21:07:56 +0530

    Add files via upload

---
+++ Storybook.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Simulator ===")
+print("""You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star! 🤩""")
+print()
+name=input("what's your name?: ")
+enemy=input("who's your worst enemy?: ")
+power=input("what's your superpower?: ")
+print(name,"just entered the grand arena and he is waiing for the man who made his life tougher.",
+      name,"is the only man standing who can defeat this bad man. This bad man is having a name and it is",
+      enemy,".",
+      name,"is going to use his",
+      power,"to defeat",
+      enemy,".")
